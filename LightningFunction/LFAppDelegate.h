d8dd339890d184732bc5c0f61f29454e18ead0d1
CA Ewerbring <caewerbring@gmail.com>
2014-05-20 10:47:28 +0200

    Init commit

---
+++ LightningFunction/LFAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  LFAppDelegate.h
+//  LightningFunction
+//
+//  Created by CAwesome on 2014-05-20.
+//  Copyright (c) 2014 CAwesome. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ LightningFunction/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LightningFunction
+//
+//  Created by CAwesome on 2014-05-20.
+//  Copyright (c) 2014 CAwesome. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
